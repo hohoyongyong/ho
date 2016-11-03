be89d0af1e5870092adb96316f9631b0e5d5918b
ho <ho@ho-PC>
2016-11-03 09:20:44 +0900

    first commit

---
+++ Super/src/CColorPoint.java
@@ -0,0 +1,19 @@
+public class CColorPoint extends Cpoint {
+
+	private String color;
+
+	public CColorPoint(int x, int y, String color) {
+		super(x, y);
+		this.color = color;
+	}
+	public	String toString() {
+		return super.toString() + color;
+	}
+	public static void main(String[] args) {
+		Cpoint a;
+		a = new Cpoint(2,3);
+		System.out.println(a);
+		a = new CColorPoint(3, 4, "red");
+		System.out.println(a);
+	}
+}
---
+++ Super/src/Cpoint.java
@@ -0,0 +1,11 @@
+
+public class Cpoint {
+	private int x, y;
+	public Cpoint(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+	public String toString(){
+		return "(" + x + "," + y + ")";
+	}
+}
